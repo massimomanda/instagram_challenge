f2996d680a09f0c119cba516afb8892822461049
Ilaria <idellaquila@sysmanagement.it>
2022-09-01 17:29:03 +0200

    progress bar

--- src/app/story-detail/story-detail.component.ts
+++ src/app/story-detail/story-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UtilityService } from '../utility.service';
 
@@ -7,58 +7,70 @@ import { UtilityService } from '../utility.service';
   templateUrl: './story-detail.component.html',
   styleUrls: ['./story-detail.component.css'],
 })
-export class StoryDetailComponent implements OnInit {
+export class StoryDetailComponent implements OnInit, AfterViewInit {
   constructor(
     public utility: UtilityService,
-    private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router:Router
   ) {}
 
   storyId: any;
+  // time!:number;
+  // interval:any
   larghezza: number = 0;
-  // width!: any;
-  // id: any = 0;
-  // i: any = 0;
+  width!: any;
+  id: any = 0;
+  i: any = 0;
+  start:any
+  
 
-  elem: any;
   @ViewChild('myBar') myBar!: ElementRef;
   ngOnInit(): void {
+    console.log('stringa')
+
+    // this.move();
+
     this.route.params.subscribe((params) => {
       this.storyId = params;
       console.log(this.storyId);
     });
-    setInterval(this.larghezzaPiuUno, 1000);
+
+    // this.interval = setInterval(() => this.timer(), 1000)
+
+    
   }
+
   ngAfterViewInit() {
     // this.elem = document.getElementById('myBar');
     // this.elem.style.width = '40%';
     console.log(this.myBar);
-    // setTimeout(() => {
-    //   this.router.navigate(['/main']);
-    // }, 1000);
-    // this.move();
-    this.myBar.nativeElement.style.width = this.larghezza + '%';
+    this.move()
+    // this.myBar.nativeElement.style.width = this.larghezza + '%';
   }
-  // move() {
-  //   if (this.i == 0) {
-  //     this.i = 1;
-  //     var larghezza = 1;
-  //     // this.width = 1;
-  //     this.id = setInterval(this.frame, 30);
-  //   }
-  //   frame() {
-  //     // this.width = 1;
-  //     if (larghezza >= 100) {
-  //       clearInterval(this.id);
-  //       this.i = 0;
-  //     } else {
-  //       larghezza + 1;
-  //       this.myBar.nativeElement.style.width = larghezza + '%';
-  //     }
-  //   }
-  // }
-  larghezzaPiuUno() {
-    this.larghezza += 1;
-    console.log(this.larghezza);
+
+
+  move() {
+    if (this.i == 0) {
+      this.i = 1;
+      this.larghezza = 6.66;
+      // this.width = 1;
+      this.start = Date.now()
+      console.log(this.start)
+      this.id = setInterval(() => this.frame(), 1000);
+      
+    }
+
+  }
+  frame() {
+    // this.width = 1;
+    if (this.larghezza >= 100) {
+      clearInterval(this.id);
+      this.router.navigate(['/main']);
+      console.log(Date.now()-this.start)
+      this.i = 0;
+    } else {
+      this.larghezza+= 6.66;
+      this.myBar.nativeElement.style.width = this.larghezza + '%';
+    }
   }
 }
